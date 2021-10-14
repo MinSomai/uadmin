e29fbce72f7184c0a65993ee7f97f8033a30098f
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2021-10-14 19:35:11 +0200

    example of how to override templates

--- examples/proofit-example/app.go
+++ examples/proofit-example/app.go
@@ -1,11 +1,19 @@
 package proofit_example
 
-import "github.com/sergeyglazyrindev/uadmin"
+import (
+	"embed"
+	"github.com/sergeyglazyrindev/uadmin"
+	"github.com/sergeyglazyrindev/uadmin/core"
+)
 
 func NewProofitApp(environment string) *uadmin.App {
 	app1 := uadmin.NewApp(environment, true)
 	// next two lines are mandatory for uadmin to determine your blueprints and everything else.
 	app1.Initialize()
 	app1.InitializeRouter()
+	core.CurrentConfig.OverridenTemplatesFS = &templatesRoot
 	return app1
 }
+
+//go:embed templates
+var templatesRoot embed.FS
